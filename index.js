f69f79a7e353a2cfa78c81b743e235b7cbd7476c
WahomeVincentMwangiclear <vinwahome@gmail.com>
2023-03-31 22:31:34 +0300

    Only displaying the alert message

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Add your code here
+
+function submitData (userName, userEmail) {
+const bodyContent = {
+    name : "Wahome Vincent",
+    email : "moringa@yahoo.ac.ke"
+}
+const fetchObject = {
+    method : "POST",
+    Headers : {
+        "Content-Type" : "application/json"
+    },
+    body : JSON.stringify(bodyContent)
+}
+
+    fetch('http://localhost:3000/users',fetchObject)
+    .then(res => res.json())
+    .then(object => console.log(bodyContent))
+    .catch(error => {
+        alert("There is an error")
+        console.log("An error somewhere")
+    })
+
+}
+
+submitData()
